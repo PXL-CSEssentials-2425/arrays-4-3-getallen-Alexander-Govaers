eb060b00e763713ae08b17f59c037e9d40b2a824
Alexander-Govaers <12401919@student.pxl.be>
2025-07-28 11:52:52 +0200

    done

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("==== SOM bepalen van 3 getallen ====");
+
+int[] getallenUser = new int[3];
+
+Console.WriteLine("Geef getal 1/3 : ");
+getallenUser[0] = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Geef getal 2/3 : ");
+getallenUser[1] = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Geef getal 3/3 : ");
+getallenUser[2] = int.Parse(Console.ReadLine());
+
+Console.ReadLine();
+
+Console.WriteLine($"De som van deze getallen is {getallenUser.Sum()} ");
